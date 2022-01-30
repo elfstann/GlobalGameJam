ed2fa232ef9b8fffa65d045ed1c45e2f863bf82c
unlugir <treedimober@gmail.com>
2022-01-30 11:59:47 +0200

    Level work in progress, OTOSHEL NA 5 min

---
+++ Assets/_Internal/Scripts/PseudoTramplin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PseudoTramplin : MonoBehaviour
+{
+    [SerializeField] int force = 20;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player") 
+        {
+            Rigidbody2D player = collision.gameObject.transform.parent.GetComponent<Rigidbody2D>();
+            player.velocity = new Vector2(player.velocity.x, force);
+        }
+    }
+}
