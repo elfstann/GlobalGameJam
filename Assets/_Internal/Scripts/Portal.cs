41e84810ccd7cc96a5d51cd4f806a454fc9dfba5
unlugir <treedimober@gmail.com>
2022-01-30 13:58:25 +0200

    Portal mechanics

--- Assets/_Internal/Scripts/Portal.cs
+++ Assets/_Internal/Scripts/Portal.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform destination;
+    public bool isActive {get;set;} = false;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (!isActive) return;
+        if (collision.gameObject.tag != "Player") return;
+        PlayerController.Instance.transform.position = destination.position;
     }
 }
