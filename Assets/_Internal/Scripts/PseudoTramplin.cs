029ac48049e4413b7e20be11e7fc75a0bd093c38
Alexander Ustymenko <ustsonder@gmail.com>
2022-01-30 14:06:43 +0200

    Jump animation for shroom

--- Assets/_Internal/Scripts/PseudoTramplin.cs
+++ Assets/_Internal/Scripts/PseudoTramplin.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class PseudoTramplin : MonoBehaviour
 {
-    [SerializeField] int force = 20;
+    [SerializeField] private int force = 20;
+    [SerializeField] private float delay = 0.5f;
+    [SerializeField] private Animator animator;
+
+    private bool isJumpAvailable = true;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player") 
         {
+            if (!isJumpAvailable) return;
+
             Rigidbody2D player = collision.gameObject.transform.parent.GetComponent<Rigidbody2D>();
-            player.velocity = new Vector2(player.velocity.x, force);
+            if (player == null) return;
+
+            StartCoroutine(JumpRoutine(player));
         }
+    }
+
+    private IEnumerator JumpRoutine(Rigidbody2D player)
+    {
+        isJumpAvailable = false;
+        player.velocity = new Vector2(player.velocity.x, force);
+        if (AudioPlayer.Instance != null) AudioPlayer.Instance.PlaySound(SoundEvent.Jump);
+        animator.SetTrigger("Jump");
+
+        yield return new WaitForSeconds(delay);
+        isJumpAvailable = true;
     }
 }
